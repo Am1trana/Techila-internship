f9312a8dc0394859a062bc1a76265dd29cb5b0ad
Amit kumar <amitkuamar.techila@gmail.com>
2024-03-15 10:48:38 +0530

    first commit

---
+++ 11 march/script.js
@@ -0,0 +1,9 @@
+function a(){
+    let inp = document.getElementById("txt").value;
+    document.getElementById("para").innerText= inp;
+}
+
+function b(){
+    let inp = document.getElementById("mail").value;
+    document.getElementById("para1").innerText= inp;
+}
